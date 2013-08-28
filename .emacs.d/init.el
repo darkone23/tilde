661ce57685534f40116fc6f0346982a755eb7bba
Thomas Omans <thomas.omans@gmail.com>
2013-08-27 19:14:04 -0700

    Adding emacs configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,26 @@
+;; Use the Marmalade Package Manager
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;; Lift the exec path from the user shell
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
+;; Ensure packages
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(ack-and-a-half better-defaults)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
